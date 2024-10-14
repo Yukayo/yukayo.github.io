e129ef073c7abc1ce667404d0808dd49ae694271
go2 <misslyysy@qq.com>
2024-10-14 21:25:55 +0900

    update

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'awards', 'news', 'academic']
+const section_names = ['home', 'news', 'publications', 'academic', 'awards']
 
 
 window.addEventListener('DOMContentLoaded', event => {
